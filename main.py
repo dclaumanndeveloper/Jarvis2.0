88a455d9087d95845fc8da398a8ec6713706a0dd
Diego Ramos Claumann <dclaumanndeveloper@gmail.com>
2025-06-23 20:22:36 -0300

    fix ui minimize and maximized from the Jarvis

--- main.py
+++ main.py
@@ -37,13 +37,14 @@ def take_query():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Ouvindo...")
-        r.pause_threshold = 1
+        r.pause_threshold = 0.5
         audio = r.listen(source)
 
     try:
         print("Reconhecendo...")
         query = r.recognize_google(audio, language='pt-BR')
         print(f"Usuário disse: {query}\n")
+        ui.showMaximized()
     except Exception as e:
         print(e)
 #        speak("Desculpe, não consegui entender. Pode repetir?")
@@ -60,41 +61,57 @@ if __name__ == '__main__':
             
             speak("Abrindo o Google...")
             webbrowser.open("https://www.google.com")
+            ui.showMinimized()
         
         elif 'que horas são' in query:
             str_time = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Senhor, agora são {str_time}")
+            ui.showMinimized()
         elif 'tocar' in query:
             tocar(query) 
-         
+            ui.showMinimized()
         elif 'aumentar volume' in query:
             aumentar_volume()
+            ui.showMinimized()
         elif 'diminuir volume' in query:
             diminuir_volume()
+            ui.showMinimized()
         elif 'definir' in query and 'volume' in query:
             definir_volume(query)
+            ui.showMinimized()
         elif 'pesquisar' in query:
             pesquisar(query)
+            ui.showMinimized()
         elif 'abrir' in query:
             abrir(query)
+            ui.showMinimized()
         elif 'verificar' in query and 'internet' in query:
             verificar_internet()
+            ui.showMinimized()
         elif 'verificar' in query and 'sistema' in query:
             system_info = get_system_info()
             for key, value in system_info.items():
                 print(f'{key}: {value}')
                 speak(f'{key}: {value}')
+
+            ui.showMinimized()
         elif 'ligar as luzes' in query:
             speak("Ligando as luzes.")
+            ui.showMinimized()
         elif 'temperatura' in query :
             buscar_temperatura()
+            ui.showMinimized()
         elif 'escreva' in query:
             escreva(query)
+            ui.showMinimized()
         elif 'iniciar dia' in query:
             start_day()
+            ui.showMinimized()
         elif 'finalizar dia' in query:
             finish_day()
+            ui.showMinimized()
         elif 'parar' in query or 'sair' in query:
             speak("Desativando. Até a próxima.")
+            ui.showMinimized()
             break
 
